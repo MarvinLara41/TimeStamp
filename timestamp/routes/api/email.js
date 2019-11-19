f065fdc630e7afcc8b6b4bc263f80d9c0383baad
marvin lara <laramarvin105@gmail.com>
2019-11-19 10:29:16 -0500

    fixed a error code on backend

--- timestamp/routes/api/email.js
+++ timestamp/routes/api/email.js
@@ -2,5 +2,5 @@ var router = require('express').Router();
 const email = require('../../controller/emailer')
 
 router.route('/email')
-        .post(email.Email);
+        .post(email.sendEmail);
 module.exports = router
