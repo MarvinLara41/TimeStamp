34b6f25e2c52eb2a4b01eec071884efbff067d28
marvin lara <laramarvin105@gmail.com>
2019-12-11 15:56:53 -0500

    server rendered public/index

--- timestamp/server.js
+++ timestamp/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(" public/index"));
+  app.use(express.static("build/index"));
 }
 
 // Add routes, both API and vie
